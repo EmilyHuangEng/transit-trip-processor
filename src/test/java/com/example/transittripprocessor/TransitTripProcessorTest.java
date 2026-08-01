ea3164c504fc59c80a7622699ec29b97aecdc988
Emily Huang <emilyhuang.dev@gmail.com>
2026-08-01 15:05:40 +1000

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/transittripprocessor/TransitTripProcessorTest.java
@@ -0,0 +1,13 @@
+package com.example.transittripprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitTripProcessorTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
