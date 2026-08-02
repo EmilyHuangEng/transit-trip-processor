727f3d6fd077c7dd12cb8717db34112187c6b528
Emily Huang <emilyhuang.dev@gmail.com>
2026-08-02 16:39:14 +1000

    Clean up

--- src/main/java/com/example/transittripprocessor/TransitTripProcessorApplication.java
+++ src/main/java/com/example/transittripprocessor/TransitTripProcessorApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransitTripProcessorApplication {
-	public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(TransitTripProcessorApplication.class, args);
-        System.out.println(">> in TransitTripProcessorApplication main ... ");
     }
 }
